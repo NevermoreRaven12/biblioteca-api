fe7f23a7e1b1b35c48e0c29f6c95fbbaf1d38010
Pedro Cardoso <tiojoaobrb@gmail.com>
2025-10-23 21:30:54 -0300

    Criadas as rotas base

--- src/main/java/com/api/Main.java
+++ src/main/java/com/api/Main.java
@@ -1,7 +1,13 @@
 package com.api;
 
+import com.api.model.Emprestimo;
+import com.api.model.Livro;
 import com.api.model.Usuario;
+import com.api.repository.EmprestimoRepository;
+import com.api.repository.LivroRepository;
 import com.api.repository.UsuarioRepository;
+import com.api.services.EmprestimoService;
+import com.api.services.LivroService;
 import com.api.services.UserService;
 
 import io.javalin.Javalin;
@@ -12,6 +18,10 @@ public class Main {
     public static void main(String[] args) {
         UsuarioRepository usuarioRepository = new UsuarioRepository();
         UserService userService = new UserService(usuarioRepository);
+        LivroRepository livroRepository = new LivroRepository();
+        LivroService livroService = new LivroService(livroRepository);
+        EmprestimoRepository emprestimoRepository = new EmprestimoRepository();
+        EmprestimoService emprestimoService = new EmprestimoService(emprestimoRepository, usuarioRepository, livroRepository);
 
         var app = Javalin.create(config -> {
             config.jsonMapper(new JavalinJackson());
@@ -30,6 +40,40 @@ public class Main {
 
         app.get("/home", ctx -> {
             ctx.redirect("/index.html");
+        });
+
+        app.get("/livros", ctx -> {
+            String author = ctx.queryParam("author");
+            String title = ctx.queryParam("titulo");
+            var livros = livroService.listarLivros().stream()
+            .filter(l -> author == null || l.getAutor().equalsIgnoreCase(author))
+            .filter(l -> title == null || l.getTitulo().equalsIgnoreCase(title))
+            .toList();
+
+            ctx.json(livros);
+        });
+        app.post("/livros", ctx -> {
+            try {
+            Livro livro = ctx.bodyAsClass(Livro.class);
+            Livro livroCadastrado = livroService.cadastrarLivro(livro.getTitulo(), livro.getAutor());
+            ctx.status(201).json(livroCadastrado);
+            } catch (IllegalArgumentException e){
+                ctx.status(400).result(e.getMessage());
+            }
+        });
+
+        app.post("/emprestimos", ctx -> {
+            Emprestimo emprestimo = ctx.bodyAsClass(Emprestimo.class);
+            Livro livro = livroService.buscarLivro(emprestimo.getLivro().getId());
+            Usuario usuario = userService.buscarUsuario(emprestimo.getUsuario().getId());
+
+            try{
+                
+                    emprestimoService.fazerEmprestimo(livro.getId(), usuario.getId());
+                    ctx.status(201).result("Emprestimo realizado.");
+            } catch (IllegalArgumentException e){
+                ctx.status(400).result(e.getMessage());
+            }
         });
     }
 }
